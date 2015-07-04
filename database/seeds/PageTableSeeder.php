4b2ec598bc22d172c0ec275e5b85aae1276f917d
YogiAi <ayb854@163.com>
2015-07-04 11:56:15 +0800

    first commit

---
+++ database/seeds/PageTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+use App\Page;
+use Illuminate\Database\Seeder;
+
+/**
+ * Created by PhpStorm.
+ * User: CIR
+ * Date: 6/29/15
+ * Time: 17:08
+ */
+
+class PageTableSeeder extends Seeder {
+
+    public function run(){
+        DB::table('pages')->delete();
+
+        for ($i=0; $i < 10; $i++) {
+            Page::create([
+                'title'   => 'Title '.$i,
+                'slug'    => 'first-page',
+                'body'    => 'Body '.$i,
+                'user_id' => 1,
+            ]);
+        }
+    }
+
+}
