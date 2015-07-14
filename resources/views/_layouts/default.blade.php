cceb5c7df694bd583a706e02d2bd81ba735d9972
YogiAi <ayb854@163.com>
2015-07-14 17:10:30 +0800

    merge

--- resources/views/_layouts/default.blade.php
+++ resources/views/_layouts/default.blade.php
@@ -44,7 +44,7 @@
 
 @yield('content')
 
-<footer class="page-footer teal">
+<footer class="page-footer light-blue darken-4">
     <div class="container">
         <div class="row">
             <div class="col l6 s12">
@@ -79,6 +79,18 @@
         </div>
     </div>
 </footer>
+
+<div class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+    <a class="btn-floating btn-large light-blue darken-4">
+        <i class="large material-icons">mode_edit</i>
+    </a>
+    <ul>
+        <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+        <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+        <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+        <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+    </ul>
+</div>
 
 </body>
 </html>
